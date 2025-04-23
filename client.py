ee321c21bbcc72bfb741d5a23f0002d3cbf28d57
Ana <acgs@cesar.school>
2025-04-23 17:55:17 -0300

    stating fase two

--- client.py
+++ client.py
@@ -3,15 +3,64 @@ import socket
 HOST = "localhost"
 PORT = 3000
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect((HOST, PORT))
 
-tipo_operacao = input("Digite o modo de operação: ")
-tamanho_maximo = input("Digite o tamanho máximo: ")
+def handShake():
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect((HOST, PORT))
 
-client.sendall(f"{tipo_operacao},{tamanho_maximo}".encode())
+    tipo_operacao = input("Digite o modo de operação: ")
+    tamanho_maximo = input("Digite o tamanho máximo: ")
 
-resposta = client.recv(1024).decode()
-print(f"Resposta do servidor: {resposta}")
+    client.sendall(f"{tipo_operacao},{tamanho_maximo}".encode())
 
-client.close()
+    resposta = client.recv(1024).decode()
+    print(f"Resposta do servidor: {resposta}")
+
+    return client
+
+def sendMessage(client):
+
+    message = input("c: ")
+    chunks = [message[i:i+3] for i in range(0, len(message), 3)]
+
+    for chunk in chunks:
+        print(chunk)
+        client.sendall(f"{chunk}".encode())
+
+    client.sendall('END'.encode())
+
+    print(chunks)
+
+    return message    
+
+
+def recieveMessage(client):
+
+    buffer = []
+
+    chunk = client.recv(1024).decode()
+
+        # if not chunk :
+        #     break
+
+    buffer.append(chunk)
+    print(f"s: {''.join(buffer)}")
+
+
+def main():
+
+    client = handShake()
+
+    print(f"A conversa entre você e o servidor começa aqui :D")
+
+    while True:
+        message = sendMessage(client)
+        recieveMessage(client)
+
+        if message == 'exit':
+            break
+
+    client.close()
+
+if __name__ == '__main__':
+    main()
--- server.py
+++ server.py
@@ -3,21 +3,68 @@ import socket
 HOST = "localhost"
 PORT = 3000
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind((HOST, PORT))
-server.listen()
-print("Aguardando conexão...")
 
-conn, addr = server.accept()
-print(f"Conectado por {addr}")
+def handShake():
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind((HOST, PORT))
+    server.listen()
+    print("Aguardando conexão...")
 
-dados = conn.recv(1024).decode()
-tipo_operacao, tamanho_maximo = dados.split(",")
-print(
-    f"Configurações recebidas do cliente: Modo de operação = {tipo_operacao}, Tamanho máximo = {tamanho_maximo}"
-)
+    conn, addr = server.accept()
+    print(f"Conectado por {addr}")
 
-conn.sendall("Configurações aplicadas com sucesso".encode())
+    dados = conn.recv(1024).decode()
+    tipo_operacao, tamanho_maximo = dados.split(",")
+    print(
+        f"Configurações recebidas do cliente: Modo de operação = {tipo_operacao}, Tamanho máximo = {tamanho_maximo}"
+    )
 
-conn.close()
-server.close()
+    conn.sendall("Configurações aplicadas com sucesso".encode())
+
+    return server, conn
+
+def reciveMessage(conn):
+
+    buffer = []
+
+    while True:
+    # for i in range(2):
+
+        chunk = conn.recv(3).decode()
+
+        if not chunk :
+            break    
+
+        buffer.append(chunk)
+        print(f"buffer: {buffer}")
+    
+    return ''.join(buffer)
+
+
+def sendMessage(conn, clientResp):
+
+    if clientResp == 'exit':
+        response = "Fechando conexão..."
+    else: 
+        response = input("s: ")
+    
+    conn.sendall(f"{response}".encode())
+
+
+def main():
+
+    server, conn = handShake()
+
+    # while True: 
+    response = reciveMessage(conn)
+
+    sendMessage(conn, response)
+
+        # if response == 'exit':
+            # break
+
+    conn.close()
+    server.close()
+
+if __name__ == '__main__':
+    main()
